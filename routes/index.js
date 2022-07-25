8e32fd07f049e710b74528e35b2a706e4256af7b
Alexander Ab <ab.alexande21@gmail.com>
2022-07-25 17:48:37 -0400

    added comment routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const { Router } = require('express')
 const router = Router()
 const controllers = require('../controllers/CarController')
+const comments = require('../controllers/CommentController')
 
 router.get('/', (req, res) => res.send('This is the root'))
 
+//Car Routes
 router.get('/cars', controllers.getAllCars)
 router.get('/cars/:id', controllers.getCarById)
 router.post('/cars', controllers.createCar)
+
+//Comment Routes
+router.get('/comments', controllers.getAllComments)
+router.create('/comments', controllers.createComment)
+router.put('/comments', controllers.updateComment)
+router.delete('/comments', controllers.deleteComment)
 
 module.exports = router
