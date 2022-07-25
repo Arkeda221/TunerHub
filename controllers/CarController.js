f5ee4e726b37d45172247cd5ac349f322bc951bd
Alexander Ab <ab.alexande21@gmail.com>
2022-07-25 14:13:52 -0400

    added controller to get cars by id

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -9,6 +9,22 @@ const getAllCars = async (req, res) => {
   }
 }
 
+const getCarById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const cars = await Cars.findById(id)
+    if (cars) {
+      return res.status(200).json({ cars })
+    }
+    return res
+      .status(404)
+      .send('Try a different Input. The one you have tried does not exist.')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  getAllCars
+  getAllCars,
+  getCarById
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const controllers = require('../controllers/CarController')
 router.get('/', (req, res) => res.send('This is the root'))
 
 router.get('/cars', controllers.getAllCars)
+router.get('/cars/:id', controllers.getCarById)
 
 module.exports = router
