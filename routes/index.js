70f89bfe008759f9b8ab25f4ccbedeb2dc4cefc6
Alexander Ab <ab.alexande21@gmail.com>
2022-07-25 17:50:35 -0400

    fixed typos

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 const { Router } = require('express')
 const router = Router()
-const controllers = require('../controllers/CarController')
-const comments = require('../controllers/CommentController')
+const carControllers = require('../controllers/CarController')
+const commentControllers = require('../controllers/CommentController')
 
 router.get('/', (req, res) => res.send('This is the root'))
 
 //Car Routes
-router.get('/cars', controllers.getAllCars)
-router.get('/cars/:id', controllers.getCarById)
-router.post('/cars', controllers.createCar)
+router.get('/cars', carControllers.getAllCars)
+router.get('/cars/:id', carControllers.getCarById)
+router.post('/cars', carControllers.createCar)
 
 //Comment Routes
-router.get('/comments', controllers.getAllComments)
-router.create('/comments', controllers.createComment)
-router.put('/comments', controllers.updateComment)
-router.delete('/comments', controllers.deleteComment)
+router.get('/comments', commentControllers.getAllComments)
+router.create('/comments', commnetControllers.createComment)
+router.put('/comments', commnetControllers.updateComment)
+router.delete('/comments', commentControllers.deleteComment)
 
 module.exports = router
