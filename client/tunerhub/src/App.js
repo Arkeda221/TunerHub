83e45bfda80a3e153c4a2c3615ea889f839a9122
Alexander Ab <ab.alexande21@gmail.com>
2022-07-26 13:47:37 -0400

    added routes to app.js

--- client/tunerhub/src/App.js
+++ client/tunerhub/src/App.js
@@ -1,5 +1,10 @@
 import './styles/App.css'
 import Nav from './components/Nav'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
   return (
@@ -7,6 +12,12 @@ function App() {
       <header>
         <Nav />
       </header>
+      <main>
+        <routes>
+          <Route path="/" element={<Home cars={cars} />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </routes>
+      </main>
     </div>
   )
 }
--- client/tunerhub/src/components/Nav.jsx
+++ client/tunerhub/src/components/Nav.jsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Nav = () => {
   return(
     <header>
+      <nav>
         <Link to="/"><button> Home </button></Link>
         <Link to="/about"><button> About </button></Link>
         <Link to="/createPost"><button> Create Post </button></Link>
+        </nav>
     </header>
   )
 }
