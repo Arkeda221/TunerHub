9b2139c1437b5a3711b079afd259e7655a3ddae0
Alexander Ab <ab.alexande21@gmail.com>
2022-07-26 11:02:58 -0400

    connected nav bar to app

--- client/tunerhub/src/App.js
+++ client/tunerhub/src/App.js
@@ -1,7 +1,14 @@
 import './styles/App.css'
+import Nav from './components/Nav'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <Nav />
+      </header>
+    </div>
+  )
 }
 
 export default App
