13b91832675e6affeff58cc3e370ef0d0832b9c4
Alexander Ab <ab.alexande21@gmail.com>
2022-07-29 12:29:13 -0400

    Revert "modified db index.js file"
    This reverts commit 92d97fabff53b32d764aa3c8a3adb7305f6e438b.

--- db/index.js
+++ db/index.js
@@ -1,24 +1,3 @@
-// const mongoose = require('mongoose')
-// require('dotenv').config()
-
-// let dbUrl =
-//   process.env.NODE_ENV === 'production'
-//     ? process.env.MONGODB_URI
-//     : `mongodb+srv://Aab221:${process.env.MONGO_PASSWORD}as@cardatabase.icohqhp.mongodb.net/CarDatabase`
-
-// mongoose
-//   .connect(dbUrl)
-//   .then(() => {
-//     console.log('Successfully connected to MongoDB!')
-//   })
-//   .catch((e) => {
-//     console.error('Connection error', e.message)
-//   })
-// mongoose.set('debug', true)
-// const db = mongoose.connection
-
-// module.exports = db
-
 const mongoose = require('mongoose')
 
 mongoose
