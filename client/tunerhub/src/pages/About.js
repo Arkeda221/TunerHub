e941ddeefa7e0ca33ba71b444183a890e9965591
Alexander Ab <ab.alexande21@gmail.com>
2022-07-31 19:13:17 -0400

    typo

--- client/tunerhub/src/pages/About.js
+++ client/tunerhub/src/pages/About.js
@@ -7,8 +7,9 @@ const About = () => {
         engineering immersive class at General Assembly. This applications is
         called 'TunerHub', and was developed using the full MERN stack. The
         purpose of TunerHub is for car enthusiasts to be able to share their
-        cars, and leave comments on other enthusiast's posts. Please be
-        respectful in the comment section.
+        cars, and leave comments on other enthusiast's posts. When uploading a
+        photo, please make sure that it is 960x1160. Please be respectful in the
+        comment section.
       </p>
     </div>
   )
