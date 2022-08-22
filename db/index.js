97f79cdbad5be7b9366e3ad2ee439749bfd37d4b
Alexander Ab <ab.alexande21@gmail.com>
2022-08-22 16:50:34 -0400

    third time is the charm

--- db/index.js
+++ db/index.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
-// require('dotenv').config()
+require('dotenv').config()
 
 mongoose
-  // .connect(process.env.MONGODB_URL)
-  .connect('mongodb://127.0.0.1:27017/CarDatabase')
+  .connect(process.env.MONGODB_URI)
+  // .connect('mongodb://127.0.0.1:27017/CarDatabase')
   .then(() => {
     console.log('Successfully connected to MongoDB')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-//mongoose.set('debug', true)
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
